57a4bc60c21409267eb18cf2dc8bf6536fadfdb0
Colin Bradley <colin.bradley89@gmail.com>
2019-04-08 09:52:06 -0400

    add spatial join streaming script

---
+++ taxi_spark.py
@@ -0,0 +1,99 @@
+
+import sys
+import pyspark
+import fiona
+import fiona.crs
+import shapely
+import rtree
+import pyproj
+import shapely.geometry as geom
+import pandas as pd
+import geopandas as gpd
+import csv
+import gzip
+import os
+
+sc = pyspark.SparkContext()
+
+neighbor_shape = sys.argv[1]
+borough_shape = sys.argv[2]
+yellow_data = sys.argv[3]
+
+yellow_data = gzip.open(sys.argv[3], 'rb')
+
+#read in spatial data
+neighborhoods = gpd.read_file(neighbor_shape).to_crs(fiona.crs.from_epsg(2263))
+
+#read in spatial data
+boroughs = gpd.read_file(borough_shape).to_crs(fiona.crs.from_epsg(2263))
+
+def processTrips(pid, records):
+    if pid==0:
+        next(records)
+    counts = {}
+    import rtree
+  
+    reader = csv.reader(records)
+    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)
+    neighborhoods = gpd.read_file(neighbor_shape).to_crs(fiona.crs.from_epsg(2263))
+    boroughs = gpd.read_file(borough_shape).to_crs(fiona.crs.from_epsg(2263))
+    index_pick = rtree.Rtree()
+    index_drop = rtree.Rtree()
+    
+    #gets the pick location neighborhood and sets up an index for it
+    for idx,geometry in enumerate(neighborhoods.geometry):
+        index_pick.insert(idx, geometry.bounds)
+    
+    #set up r-tree index for drop off location
+    for idx,geometry in enumerate(boroughs.geometry):
+        index_drop.insert(idx, geometry.bounds)
+    
+    for row in reader:
+        try:
+            p_pick = geom.Point(proj(float(row[3]), float(row[2]))) #pick-up location lat / lon
+            p_drop = geom.Point(proj(float(row[5]), float(row[4]))) #drop off
+            match_pick = None
+            for idx in index_pick.intersection((p_pick.x, p_pick.y, p_pick.x, p_pick.y)):
+                
+                shape = neighborhoods.geometry[idx]
+                
+                if shape.contains(p_pick):
+                    match_pick = idx
+                    break
+
+            match_drop = None
+            for idx in index_drop.intersection((p_drop.x, p_drop.y, p_drop.x, p_drop.y)):
+                
+                shape = boroughs.geometry[idx]
+                
+                if shape.contains(p_drop):
+                    match_drop = idx
+                    break
+
+            if match_pick is None or match_drop is None:
+                pass
+            else:
+                match = (match_pick,match_drop)
+                counts[match] = counts.get(match, 0) + 1
+        
+        except ValueError:
+            pass 
+
+    return counts.items()
+            
+rdd = sc.textFile(yellow_data)
+counts = rdd.mapPartitionsWithIndex(processTrips) \
+            .reduceByKey(lambda x,y: x+y) \
+    .collect()
+    
+countsPerNeighborhood = list(map(lambda x: (x[0][0], x[0][1], x[1]), counts))
+mapped_counts = list(map(lambda x: (neighborhoods['neighborhood'][x[0]], boroughs['boroname'][x[1]], x[2]), countsPerNeighborhood))
+df = pd.DataFrame(mapped_counts).sort_values(2,ascending=False)
+
+borough_list = ['Staten Island','Manhattan','Brooklyn','Queens','Bronx']
+
+for i in borough_list:
+    df_filt = df[df[1]==i]
+    df_filt.sort_values(2,ascending=False,inplace=True)
+    top_neighbs = list(df_filt[0].head(3))
+    print("Top Neighborhoods for "+i+ ": {}".format(top_neighbs))
